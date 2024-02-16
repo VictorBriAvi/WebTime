e2d8ff7e8283860bc98d78bce97629e0cf5d67be
Victor Avila <victor_15_avila@hotmail.es>
2024-02-16 04:36:39 -0300

    Agrego de ws

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Precios from "./components/Precios";
 import Barberia from "./pages/Barberia";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 function App() {
+  const mensaje = (
+    <div>
+      Hola, bienvenidos ! 🤝
+      <br />
+      ¿En qué podemos ayudarte?
+    </div>
+  );
   return (
     <>
       <Navbar />
@@ -17,6 +25,18 @@ function App() {
         <Route path="/WebTime/precios" element={<Precios />} />
         <Route path="/WebTime/barberia" element={<Barberia />} />
       </Routes>
+      <FloatingWhatsApp
+        phoneNumber="+5491126875506"
+        accountName="Time For You"
+        avatar=".https://victorbriavi.github.io/WebTime/assets/Logos/LogotipoOriginalPNG.png"
+        chatMessage={mensaje}
+        placeholder="Envianos un mensaje"
+        chatboxStyle={{ backgroundColor: "#000" }}
+        statusMessage="Tiempo de espera en responder 10 minutos"
+        allowEsc={true}
+        allowClickAway={true}
+        enableClickAway={false}
+      />
       <Footer />
     </>
   );
